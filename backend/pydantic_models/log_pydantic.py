30161076bbecb488fe613c4ce297d75585732c02
MercuRY0l <udgitv@gmail.com>
2026-05-29 16:14:39 +0300

    Исправлены модели авторизации и регистрации

--- backend/pydantic_models/log_pydantic.py
+++ backend/pydantic_models/log_pydantic.py
@@ -1,6 +1,13 @@
-
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
 class LoginUser(BaseModel):
-    login : str
-    password : str
+    email: EmailStr = Field(
+        ...,
+        max_length=255
+    )
+
+    password: str = Field(
+        ...,
+        min_length=6,
+        max_length=128
+    )
