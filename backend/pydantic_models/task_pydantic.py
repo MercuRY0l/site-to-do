4fbe17714216138f0271832a4c3233661e890795
MercuRY0l <udgitv@gmail.com>
2026-06-02 14:35:08 +0300

    Добавлен pydantic для обновления task

--- backend/pydantic_models/task_pydantic.py
+++ backend/pydantic_models/task_pydantic.py
@@ -1,8 +1,15 @@
 from pydantic import BaseModel,Field
 from datetime import datetime
+from typing import Optional
 
 class TaskCreate(BaseModel):
     priority: str = Field(default="p0")
     title: str = Field(..., min_length=1, max_length=255)
     description: str = Field(..., min_length=1, max_length=512)
     date: datetime
+    
+class TaskUpdate(BaseModel):
+    priority: Optional[str] = None
+    title: Optional[str] = None
+    description: Optional[str] = None
+    date: Optional[datetime] = None
