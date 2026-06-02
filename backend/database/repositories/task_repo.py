714eedcb76bec415dcac7dfa2aa0f2f03fa7933c
MercuRY0l <udgitv@gmail.com>
2026-06-02 14:35:54 +0300

    Добавлен self ко всем методам, убран не нужный await для session.add, исправил метод update

--- backend/database/repositories/task_repo.py
+++ backend/database/repositories/task_repo.py
@@ -4,30 +4,30 @@ from ..models.task import Tasks
 from sqlalchemy import select, delete, update as sql_update
 
 class TaskRepository():
-    async def create(priority : str, title : str, description : str, date : datetime) -> Tasks:
+    async def create(self, priority : str, title : str, description : str, date : datetime) -> Tasks:
         async with async_session() as session:
             task = Tasks(priority=priority, title=title, description=description, date=date)
-            await session.add(task)
+            session.add(task)
             await session.commit()
             await session.refresh(task)
             return task
             
     
-    async def delete(id : int):
+    async def delete(self, id : int):
         async with async_session() as session:
             stmt = delete(Tasks).where(Tasks.id == id)
             res = await session.execute(stmt)
             await session.commit()
             return res.rowcount
     
-    async def update(id : int, **data):
+    async def update(self, id : int, **data):
         async with async_session() as session:
-            stmt = sql_update(Tasks).where(Tasks.id == Tasks.id).values(**data)
+            stmt = sql_update(Tasks).where(Tasks.id == id).values(**data)
             await session.execute(stmt)
             await session.commit()
             return True
     
-    async def get_by_id(id : int) -> Tasks:
+    async def get_by_id(self, id : int) -> Tasks:
         async with async_session() as session:    
             stmt = select(Tasks).where(Tasks.id == id)
             res = await session.execute(stmt)
