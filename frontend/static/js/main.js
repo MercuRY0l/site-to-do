fdd1c25ca6ce19765fe80a5593830bf085cca446
MercuRY0l <udgitv@gmail.com>
2026-06-04 10:54:04 +0300

    Добавлен initTaskPage

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,6 +1,10 @@
 
 import {initializeAuth} from "./auth/initializeAuth.js"
+import { initTaskPage } from "./tasks/initTaskPage.js";
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
     initializeAuth();
+    initTaskPage();
+    
 })
