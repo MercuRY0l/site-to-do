a89074e1b23e3432a95cce27147716cce1484d35
MercuRY0l <udgitv@gmail.com>
2026-06-04 10:15:17 +0300

    Добавил кнопку редактирования задачи

--- frontend/static/js/tasks/loadTasks.js
+++ frontend/static/js/tasks/loadTasks.js
@@ -40,8 +40,22 @@ export async function loadTasks() {
                     <span>${task.title}</span>
                 </label>
 
-                <div class="tag ${task.priority}">
-                    ${task.priority.replace("p", "")}
+                <div class="task-actions">
+                    <div class="tag ${task.priority}">
+                        ${task.priority.replace("p", "")}
+                    </div>
+                    <button class="edit-task-btn" data-id="${task.id}">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="18"
+                        height="18"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2">
+                        <path d="M17 3a2.85 2.85 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"/>
+                    </svg>
+                </button>
                 </div>
             `;
 
