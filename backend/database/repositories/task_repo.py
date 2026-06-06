c1dad923b6b303ffa3fe94313248c7ae5b32ff83
MercuRY0l <udgitv@gmail.com>
2026-06-06 20:01:01 +0300

    Добавлен метод репозитория для поиска текущих задач на день

--- backend/database/repositories/task_repo.py
+++ backend/database/repositories/task_repo.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+from datetime import datetime,time
 from ..connect import async_session
 from ..models.task import Tasks
 from sqlalchemy import select, delete, update as sql_update
+
 
 class TaskRepository():
     async def create(self, user_id : int ,priority : str, title : str, description : str, date : datetime) -> Tasks:
@@ -38,4 +39,14 @@ class TaskRepository():
             stmt = select(Tasks).where(Tasks.user_id == user_id)
             res = await session.execute(stmt)
             return res.scalars().all()
+    
+    async def get_today(self, user_id : int) -> Tasks:
+        async with async_session() as session:
             
+            today = datetime.today().date()
+            start = datetime.combine(today, time.min)
+            end = datetime.combine(today, time.max)
+            
+            stmt = select(Tasks).where(Tasks.user_id == user_id, Tasks.date.between(start,end))
+            res = await session.execute(stmt)
+            return res.scalars().all()
