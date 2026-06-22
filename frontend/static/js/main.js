96ecf0c9d8376570067706b5323ee7a4aabcbf47
MercuRY0l <udgitv@gmail.com>
2026-06-22 10:49:00 +0300

    Добавлена инициализация модальных окон и поиска в main js файл

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -5,11 +5,14 @@ import {initTodayTaskPage} from "./tasks/today/initTodayTaskPage.js"
 import {initFutureTaskPage} from "./tasks/future/initFutureTask.js"
 import {initReportsPage} from "./report_page/initReportsPage.js"
 import { setActiveMenuItem } from "./modules/setActiveMenuItem.js";
-
+import { initSearch } from "./search/initSearch.js";
+import { initModals } from "./modules/modals.js";
 
 document.addEventListener("DOMContentLoaded", async()=>{
     initializeAuth();
     setActiveMenuItem();
+    initSearch();
+    initModals();
 
     initTaskPage().catch(err =>
         console.error("Task page error:", err)
